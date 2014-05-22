9bbca86d2726606279741321ca593ca3f34778cf
Rodrigo Dirk <rodrigodirk@nasajon.com.br>
2014-05-22 16:44:38 -0300

    Terminada a configuração do SAML

--- src/Nasajon/CalendarioBundle/Controller/DefaultController.php
+++ src/Nasajon/CalendarioBundle/Controller/DefaultController.php
@@ -3,6 +3,8 @@
 namespace Nasajon\CalendarioBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
 
 class DefaultController extends Controller
 {
@@ -10,4 +12,13 @@ class DefaultController extends Controller
     {
         return $this->render('NasajonCalendarioBundle:Default:index.html.twig');
     }
+    public function helloAction(Request $request)
+    {
+
+	//var_dump($this->get('security.context'));
+	var_dump($this->getUser());
+	var_dump($this->get('security.context')->isGranted('IS_AUTHENTICATED_ANONYMOUSLY'));
+        return $this->render('NasajonCalendarioBundle:Default:index.html.twig');
+    }
+
 }
---
+++ src/Nasajon/CalendarioBundle/Controller/LoginController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Nasajon\CalendarioBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\SecurityContext;
+
+
+class LoginController extends Controller
+{
+    public function indexAction(Request $request)
+    {
+      
+        
+    }
+    public function logoutAction(Request $request)
+    {
+
+        
+    }
+
+}
+ 
